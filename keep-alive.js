bf88af764480aa7c12a4d32f5221079f2730ea47
MD. AMINUL SARDAR <234582242+MDAMINULSARDAR@users.noreply.github.com>
2025-10-03 12:00:47 +0600

    Update keep-alive.js

--- keep-alive.js
+++ keep-alive.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT ||3000;
-//const port = 3000;
-app.get('/', (req, res) =>)
-res.send('Remade By MrTomXxX!!');
+const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log('your app is listening a http://localhost:${port}');
+app.get('/', (req, res) => {
+  res.send('Remade Aminul Sardar !!');
+});
+
+app.listen(port, () => {
+  console.log(`Your app is listening at http://localhost:${port}`);
+});
